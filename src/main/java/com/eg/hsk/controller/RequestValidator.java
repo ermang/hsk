435c8872ca5cef6e94e25f6ee13b6169797508a5
erman gonul <ermangonul17@gmail.com>
2022-08-28 22:31:21 +0300

    add: RequestValidator createReservationDto

--- src/main/java/com/eg/hsk/controller/RequestValidator.java
+++ src/main/java/com/eg/hsk/controller/RequestValidator.java
@@ -1,7 +1,10 @@
 package com.eg.hsk.controller;
 
 import com.eg.hsk.dto.in.CreateReservationDto;
+import com.eg.hsk.misc.Constant;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 
 @Component
 public class RequestValidator {
@@ -10,7 +13,17 @@ public class RequestValidator {
 
         if (!dto.reservationEnd.isAfter(dto.reservationBegin))
             throw new IllegalArgumentException("reservationEnd must be later than reservationBegin");
+
+        if (dto.reservationBegin.isAfter(LocalDateTime.now()))
+            throw new IllegalArgumentException("reservationBegin must be later than now");
+
+        if (dto.reservationBegin.getMinute() % Constant.MIN_RESERVATION_DURATION_IN_MINUTES !=0)
+            throw new IllegalArgumentException("reservationBegin must be 0 or " + Constant.MIN_RESERVATION_DURATION_IN_MINUTES);
+
+        if (dto.reservationEnd.getMinute() % Constant.MIN_RESERVATION_DURATION_IN_MINUTES !=0)
+            throw new IllegalArgumentException("reservationEnd must be 0 or " + Constant.MIN_RESERVATION_DURATION_IN_MINUTES);
     }
+
 
 
 }
